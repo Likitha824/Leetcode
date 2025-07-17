4b2a004d2e764e4a7e6556ea5f6a8cfb568afc87
Likitha <65528573+Likitha824@users.noreply.github.com>
2025-07-17 16:10:26 -0400

    Time: 83 ms (69.92%) | Memory: 17.7 MB (68.29%) - LeetSync

--- 1276-closest-divisors/closest-divisors.py
+++ 1276-closest-divisors/closest-divisors.py
@@ -1,9 +1,12 @@
 class Solution:
     def closestDivisors(self, num: int) -> List[int]:
         def finddivisors(x):
+            divisors=[]
             for j in range(int(math.sqrt(x)),0,-1):
                 if x%j==0:
-                    return [j,x//j]
+                    divisors.append(j)
+                    divisors.append(x//j)
+                    return divisors
         res1=finddivisors(num+1)
         res2=finddivisors(num+2)
         if abs(res1[0]-res1[1])<abs(res2[0]-res2[1]):
