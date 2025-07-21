f0807f8f667d53e73fd54dc39b224224ef5641ce
Likitha <65528573+Likitha824@users.noreply.github.com>
2025-07-21 18:03:04 -0400

    Time: 571 ms (11.27%) | Memory: 27.8 MB (5.14%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -1,4 +1,27 @@
+#method 2 using stack
 class Solution:
+    def makeFancyString(self, s: str) -> str:
+        stack=[]
+        for char in s:
+            if stack and stack[-1][0]==char:
+                stack[-1][1]+=1
+                if stack[-1][1]>=3:
+                    stack[-1][1]=2
+            else:
+                stack.append([char,1])
+            #print(stack)
+        res="".join(char*count for char,count in stack)
+        return res           
+
+
+
+
+
+
+
+
+#first method
+'''class Solution:
     def makeFancyString(self, s: str) -> str:
         output=""
         for char in s:
@@ -6,4 +29,4 @@ class Solution:
                 continue
             else:
                 output+=char
-        return output
+        return output'''
