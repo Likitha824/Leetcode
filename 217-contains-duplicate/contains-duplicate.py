7648de3db5482efc426eec80a537f893b16f6640
Likitha <65528573+Likitha824@users.noreply.github.com>
2025-07-22 19:06:55 -0400

    Update contains-duplicate.py

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,3 +4,10 @@ class Solution:
             return True
         else:
             return False
+
+            
+            
+#three methods
+#1.using two for loops(o(n2)) and o(1)
+#2.using sort method sort and check two consequeitive elements time (o(nlogn)) space o(1)
+#3.set time and space (o(n))
