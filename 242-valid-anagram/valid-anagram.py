3c7fd6f52e36e6be995dfe38e9344e7bb0f640be
Likitha <65528573+Likitha824@users.noreply.github.com>
2025-07-23 15:16:43 -0400

    Time: 11 ms (72.29%) | Memory: 17.9 MB (64.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,15 +5,17 @@ class Solution:
         if len(t)!=len(s):
             return False
         for char in s:
-            if char not in hash1:
+            hash1[char]=hash1.get(char,0)+1
+            '''if char not in hash1:
                 hash1[char]=1
             else:
                 hash1[char]+=1
-        #print(hash1)
+        #print(hash1)'''
         for char in t:
-            if char not in hash2:
+            '''if char not in hash2:
                 hash2[char]=1
             else:
-                hash2[char]+=1
+                hash2[char]+=1'''
+            hash2[char]=hash2.get(char,0)+1
         #print(hash2)
         return hash1==hash2
